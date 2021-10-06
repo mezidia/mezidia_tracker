e1a365ef4f9ded4f6db9a861d904eee33a420d32
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-06 13:32:28 +0300

    Show room to primary key

--- base/views.py
+++ base/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
 
 
 def index(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 
-def room(request):
+def room(request, pk):
     return render(request, 'base/room.html')
