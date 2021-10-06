d65bb18b8319d954a6472b73afef7261e499aeeb
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-06 13:28:37 +0300

    Create url for room

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('room/<str:pk>/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'base/home.html')
+
+
+def room(request):
+    return render(request, 'base/room.html')
