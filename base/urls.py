4b54b62a42ba273c9d34ea874b1c02573e75dd74
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-08 09:23:33 +0300

    Some updates

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='index'),
     path('room/<str:pk>/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 
-def index(request):
+def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms': rooms})
 
 
 def room(request, pk):
-    room = None
-    for room_object in rooms:
-        if room_object['id'] == int(pk):
-            room = room_object
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
