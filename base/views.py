ec4b4b7e8a04e1965c056e3a974f09174d379332
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-09 21:50:02 +0300

    Some updates

--- base/views.py
+++ base/views.py
@@ -29,3 +29,24 @@ def create_room(request):
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'base/room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
