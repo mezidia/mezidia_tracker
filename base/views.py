e375d07c49712bdcfce9cd41e91de77283e9e6d6
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-12 12:09:04 +0300

    Show mount of rooms

--- base/views.py
+++ base/views.py
@@ -19,7 +19,9 @@ def home(request):
         Q(description__icontains=q)
     )
     topics = Topic.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms, 'topics': topics})
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
