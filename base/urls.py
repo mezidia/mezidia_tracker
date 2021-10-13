4dfeb1a029f2e8fea31fcecde956ebe0470f66f3
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-13 17:51:12 +0300

    Render login page

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>', views.update_room, name='update-room'),
--- base/views.py
+++ base/views.py
@@ -10,6 +10,10 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend developers'},
 # ]
 
+def login_page(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
