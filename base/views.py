37a16ccc2bb54d2e3f3cb227fd948c3165029670
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-14 21:10:59 +0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
@@ -20,6 +21,13 @@ def login_page(request):
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is incorrect')
     context = {}
     return render(request, 'base/login_register.html', context)
 
