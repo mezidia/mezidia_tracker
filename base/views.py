740f489bb77c9d54afa0f47314675e396b37b38d
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-18 09:41:21 +0300

    Show participants

--- base/views.py
+++ base/views.py
@@ -76,7 +76,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
@@ -84,7 +84,7 @@ def room(request, pk):
             body=request.POST.get('body')
         )
         return redirect('room', pk=room.id)
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
