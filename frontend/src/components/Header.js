a8fc870a340bd3767fe053d1afe8861bf9b96d33
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-01-14 11:35:59 +0200

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 style={{color: 'red', backgroundColor: 'blue'}}>{title}</h1>
     </header>
   )
 }
