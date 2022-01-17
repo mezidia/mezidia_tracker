b7e0c021995ac37c8468026029d3f6341d7ab634
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-01-17 15:15:15 +0200

    Some updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-
 function App() {
   return (
     <div className="container">
-      <Header title='Hello'/>
-      <Tasks/>
+      Hello world
     </div>
   );
 }
