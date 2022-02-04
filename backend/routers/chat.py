aa86cbc04b6beb32a4690f0872a0800705d86738
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-04 15:40:08 +0200

    ❄️ #9 Trying to get messages from DB ❄️

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, status, Body
+from fastapi import APIRouter, WebSocket, HTTPException, WebSocketDisconnect, status, Body
 from fastapi.responses import HTMLResponse
 
 from models import ChatModel
@@ -29,6 +29,24 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
+
+            var client_id = Date.now()
+            var chat_name = "mezidia-tracker"
+
+            url = `127.0.0.1:8000/chat/${chat_name}`;
+            let response = await fetch(url);
+            chat = await response.json();
+
+            function loadMessages() {
+                for (key of chat['messages']) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(`${key['user_id']}: ${key['content']}`)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                }
+            }
+
             var client_id = Date.now()
             var chat_name = "mezidia-tracker"
             document.querySelector("#ws-id").textContent = client_id;
@@ -75,12 +93,12 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-@router.get("")
+@router.get('')
 async def get() -> HTMLResponse:
     return HTMLResponse(html)
 
 
-@router.post('', response_description='Add new chat', response_model=ChatModel,
+@router.post('/create', response_description='Add new chat', response_model=ChatModel,
              status_code=status.HTTP_201_CREATED)
 async def create_chat(chat: ChatModel = Body(...)):
     """
@@ -93,7 +111,24 @@ async def create_chat(chat: ChatModel = Body(...)):
     return result
 
 
-@router.get('s', response_description='List all chats', response_model=List[ChatModel],
+@router.get('/{name}', response_description='Get a single chat', response_model=ChatModel,
+            status_code=status.HTTP_200_OK)
+async def get_chat_by_name(name: str):
+    """
+    Get a chat by name:
+    - **name**: chat name
+    """
+    client = Client(DB_PASSWORD, 'chats')
+
+    try:
+        data = await client.get_object({'chat_name': name})
+    except:
+        raise HTTPException(status_code=404, detail='Set some parameters')
+
+    return data
+
+
+@router.get('/all', response_description='List all chats', response_model=List[ChatModel],
             status_code=status.HTTP_200_OK)
 async def list_chats():
     """
